14fd78cf5769c38adb2d6f230907d19d349c4ed4
niklas <niklas.tscheppe@gmail.com>
2026-01-29 10:40:15 +0100

    Minimal app: heading + 4 nav buttons

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <header className="header">
+        <h1>Body Tracker</h1>
+      </header>
+      <main className="content"></main>
+      <nav className="navbar">
+        <button>Log</button>
+        <button>Stats</button>
+        <button>Phases</button>
+        <button>Settings</button>
+      </nav>
+    </div>
+  )
+}
+
+export default App
